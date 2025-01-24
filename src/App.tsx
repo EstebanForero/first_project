340d0af1f1422d902123738b40ac10447180bdc4
Esteban Forero <estebanmff@outlook.com>
2025-01-24 14:10:41 -0500

    register

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react"
 
 function App() {
+
+  const [page, setPage] = useState<>(second)
 
   return (
     <div className="bg-gray-800 w-full h-screen p-4">
       <h1 className="text-white text-center font-bold text-2xl">Club sabana</h1>
 
-
+      {}
     </div>
   )
 }
---
+++ src/data/pages.ts
@@ -0,0 +1,3 @@
+
+
+export type Pages = 'Main' | 'Register' | 'Logging'
