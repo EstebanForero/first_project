2d11ad9a68a3e367d5f5679b704d8ef3ad3e1487
Esteban Forero <estebanmff@outlook.com>
2025-01-24 14:07:09 -0500

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@
 function App() {
 
   return (
-    <>
-      <h1 className="font-bold">idk</h1>
-    </>
+    <div className="bg-gray-800 w-full h-screen p-4">
+      <h1 className="text-white text-center font-bold text-2xl">Club sabana</h1>
+    </div>
   )
 }
 
