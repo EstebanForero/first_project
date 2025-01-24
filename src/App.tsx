d644579265abae03d956d2206839eca00ea4ba9d
Esteban Forero <estebanmff@outlook.com>
2025-01-24 14:08:20 -0500

    Register component added

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="bg-gray-800 w-full h-screen p-4">
       <h1 className="text-white text-center font-bold text-2xl">Club sabana</h1>
+
+
     </div>
   )
 }
