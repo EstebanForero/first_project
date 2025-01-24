362ff5db645e92c6fe7c27373d136f3ca7e6cba9
dmartin110620 <109118261+dmartin110620@users.noreply.github.com>
2025-01-24 14:30:41 -0500

    home page

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,9 +1,26 @@
 
 import React from 'react'
 
+import { useNavigate } from 'react-router-dom';  
+
 const MainPage = () => {
+  const navigate = useNavigate();
+  const handleInscription = () => {
+        navigate('/register');
+    };
+    const handleSearch = () => {
+        navigate('/register');
+    };
   return (
-    <div>MainPage</div>
+    <div className='main-container'>
+      <h1>Sabana Club</h1>
+      <p>Este es el club de sabana</p>
+      <div>
+        <button onClick={handleInscription} className="btn-inscribir">Inscribir</button>
+        <button onClick={handleSearch} className="btn-inscribir">Buscar</button>
+      </div>
+    </div>
+    
   )
 }
 
